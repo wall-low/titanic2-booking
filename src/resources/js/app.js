2e09464cb0fa4a2806c4ecbd09b450eb3ce8b1d6
justaguy0-0 <smelyj50@gmail.com>
2025-11-12 10:13:08 +0400

    Перенес  со страниц в отдельные файлы, обновление app.css

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,12 +1,8 @@
-import '../css/app.css';
+/* Bootstrap JS */
+import 'bootstrap';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap'; // js
-
-import './bootstrap';
-
+/* Alpine.js */
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
-
 Alpine.start();
--- src/resources/views/admin/admin.blade.php
+++ src/resources/views/admin/admin.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Админ-панель')</title>
-    <!-- Scripts -->
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     {{-- Дополнительные стили --}}
